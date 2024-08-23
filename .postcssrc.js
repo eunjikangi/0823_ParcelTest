5213c5574176cf5f88b350f99b5ebf3133ac2804
eunjikangi <miki2323@naver.com>
2024-08-23 09:47:28 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,14 @@
+// 구성옵션? 숨김파일?
+
+// ESM : 브라우저에서 사용
+// Common JS : Node.js 환경에서 사용
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
